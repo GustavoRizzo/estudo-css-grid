e5f9c1c7aedd4b36dcb22f426358c760bc8f261f
Gustavo Rizzo <grizzo.albu@gmail.com>
2022-09-06 19:57:51 -0300

    Create the firts CSS Grid Exemple

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import GridTeste1 from './components/GridTeste1/GridTeste1'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -16,17 +16,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <GridTeste1></GridTeste1>
+      
     </div>
   )
 }
---
+++ src/components/GridTeste1/GridTeste1.tsx
@@ -0,0 +1,16 @@
+import './style.scss'
+
+export default function GridTeste1() {
+    return (
+        <main>
+            <header>Header</header>
+
+            <article>Article</article>
+            <aside>Author</aside>
+            <nav>Sidebar</nav>
+
+            <footer>Footer</footer>
+        </main>
+    );
+}
+
